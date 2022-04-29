25120b8c6bdaa5381ac318e9bf944006aef751bb
mndacium <vladoscompany@gmail.com>
2022-04-29 13:52:53 +0300

    <3

--- FunGuide/Shared/Sportsman.cs
+++ FunGuide/Shared/Sportsman.cs
@@ -10,10 +10,12 @@ namespace FunGuide.Shared
     public class Sportsman : BaseEntity
     {
         [Required(ErrorMessage = "The first name must be provided")]
+        [RegularExpression(@"[a-zA-Z'А-ЩЬЮЯҐЄІЇа-щьюяґєії ыЫ]{2,20}$", ErrorMessage = "Characters are not allowed.")]
         [StringLength(20, ErrorMessage = "length of First Name must be between {2} and {1}.", MinimumLength = 2)]
         public string FirstName { get; set; } = String.Empty;
 
         [Required(ErrorMessage = "The first name must be provided")]
+        [RegularExpression(@"[a-zA-Z'А-ЩЬЮЯҐЄІЇа-щьюяґєії ыЫ]{2,20}$", ErrorMessage = "Characters are not allowed.")]
         [StringLength(20, ErrorMessage = "{0} length of Second name must be between {2} and {1}.", MinimumLength = 2)]
         public string LastName { get; set; }= String.Empty;
 
@@ -29,13 +31,14 @@ namespace FunGuide.Shared
 
         [Range(35, 200, ErrorMessage = "Weight must be between {1} and {2} kilos")]
         public double Weight { get; set; }
+        [RegularExpression(@"^[a-zA-Z''-'\s]{2,20}$", ErrorMessage = "Characters are not allowed.")]
         public string? Citizenship { get; set; }
 
         [Required(ErrorMessage ="Sport must me required")]
         public Sport? Sport { get; set; }
         public int SportId { get; set; }
 
-        [StringLength(20, ErrorMessage = "Length of Team Name must be between less than {1}.")]
+        [StringLength(20, ErrorMessage = "Length of Team Name must be less than {1}.")]
         public string? Team { get; set;}
 
     }
